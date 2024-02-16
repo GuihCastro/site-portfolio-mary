f6c42f3a8b7b1c8f1f5ba78d48de769fbde98c0f
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-02-16 13:55:16 -0300

    Update ContactForm component with new styles and form elements

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 export function ContactForm() {
     return (
         <Container>
-            <h2>Contact Form</h2>
+            <h2>Contact</h2>
+
+            <Form>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="E-mail" />
+                <textarea placeholder="Message" />
+                <button type="submit">Send</button>
+            </Form>
         </Container>
     );
 };
--- src/components/ContactForm/styles.js
+++ src/components/ContactForm/styles.js
@@ -5,4 +5,59 @@ export const Container = styled.div`
 
     width: 100%;
     height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    > h2 {
+        font-size: 3rem;
+        letter-spacing: .2rem;
+        margin-bottom: 2rem;
+    }
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+    max-width: 70rem;
+    padding: 2rem;
+
+    > input, textarea {
+        width: 100%;
+        padding: 1rem;
+        border: 1px solid #000;
+        border-radius: .5rem;
+        font-size: 1.6rem;
+        font-weight: bold;
+        letter-spacing: .2rem;
+        line-height: 1.5;
+    }
+
+    > textarea {
+        resize: none;
+        height: 25rem;
+    }
+
+    > button {
+        width: 100%;
+        padding: 1rem;
+        border: none;
+        border-radius: .5rem;
+        background-color: ${({ theme }) => theme.COLORS.TEXT};
+        color: ${({ theme }) => theme.COLORS.BACKGROUND};
+        font-size: 2rem;
+        letter-spacing: .2rem;
+        font-weight: bold;
+        text-transform: uppercase;
+        cursor: pointer;
+        transition: .3s;
+
+        &:hover {
+            background-color: ${({ theme }) => theme.COLORS.SELECTION};
+        }
+    }
 `;
