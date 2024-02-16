6d6904050835814c06400c7bbe566f3e5cdd90cd
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-02-16 15:00:59 -0300

    Add emailjs library and implement contact form functionality

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,14 +1,63 @@
+import { useState } from "react";
 import { Container, Form } from "./styles";
+import emailjs from "@emailjs/browser";
 
 export function ContactForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!name || !email || !message) {
+            return alert("Please, fill in all fields!");
+        }
+
+        const templateParams = {
+            from_name: name,
+            email: email,
+            message: message,
+            to_name: 'Mariana',
+        };
+
+        emailjs.send("service_g8t2ci6", "template_di3v2bi", templateParams, "xeFTV5z4t9ZDMjKyy")
+        .then((response) => {
+            alert("Message sent successfully!");
+            console.log('SUCCESS!', response.status, response.text);
+            setName("");
+            setEmail("");
+            setMessage("");
+        }, (error) => {
+            alert("An error occurred, please try again later.", error.text);
+        });
+    }
+
     return (
         <Container>
             <h2>Contact</h2>
 
-            <Form>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="E-mail" />
-                <textarea placeholder="Message" />
+            <Form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Name" 
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+
+                <input 
+                    type="email" 
+                    placeholder="E-mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+
+                <textarea 
+                    placeholder="Message"
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
+                />
+
                 <button type="submit">Send</button>
             </Form>
         </Container>
